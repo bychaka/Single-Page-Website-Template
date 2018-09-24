3dfe90104ec1114451f0a079092b56f00be71477
Андрей <32jandarmeria32@mail.ru>
2018-09-24 21:53:47 +0300

    image-webpack-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const LiveReloadPlugin = require('webpack-livereload-plugin');
+const autoprefixer = require('autoprefixer');
 const webpack = require('webpack');
 const path = require('path');
 module.exports = {
@@ -13,12 +14,36 @@ module.exports = {
     watch:true,
     module: {
       rules: [{
-        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        test: /\.(woff|woff2|eot|ttf)$/,
         use: [
                 {
-                    loader: 'file-loader?name=./assets/fonts/[name].[ext]'
+                    loader: 'file-loader?name=./styles/fonts/[name].[ext]'
                 }
               ]
+        },
+        {test: /\.(gif|png|jpe?g|svg)$/i,
+          use: [
+            'file-loader',
+            {
+              loader: 'image-webpack-loader',
+              options: {
+                bypassOnDebug: true, // webpack@1.x
+                disable: true, // webpack@2.x and newer
+                mozjpeg: {
+                  progressive: true,
+                  quality: 75
+                },
+                // optipng.enabled: false will disable optipng
+                optipng: {
+                  enabled: false,
+                },
+                pngquant: {
+                  quality: '65-90',
+                  speed: 4
+                },
+              },
+            },
+          ],
         },
         {test: /\.scss$/,
         use: [{
@@ -26,6 +51,16 @@ module.exports = {
         }, {
           loader: "css-loader"
         }, {
+          loader: 'postcss-loader',
+          options: {
+              plugins: [
+                  autoprefixer({
+                      browsers:['edge >= 17', 'firefox >= 62', 'chrome >= 69', 'last 1 version']
+                  })
+              ],
+              sourceMap: true
+          }
+        },{
           loader: "sass-loader",
           options: {
             includePaths: []
